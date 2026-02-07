23d0b601f5c70078072010e280874c5942029715
Hyuntae Park <hyuntae.park.j@gmail.com>
2026-02-07 17:20:57 -0500

    Delete business_analytics/03_product_mix_analysis/result/20_top_products_by_revenue.sql

--- business_analytics/03_product_mix_analysis/result/20_top_products_by_revenue.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  stock_code,
-  COALESCE(product_description, '(unknown)') AS product_description,
-  SUM(quantity)                              AS units_sold,
-  SUM(revenue)                               AS revenue
-FROM dw.v_sales_enriched
-GROUP BY 1, 2
-ORDER BY revenue DESC
-LIMIT 20;
