c24bc4397ce21e56430e302c6bbb88386f75a959
Hyuntae Park <hyuntae.park.j@gmail.com>
2026-02-08 15:16:39 -0500

    Delete business_analytics/03_product_mix_analysis/sql/04_country_customer_revenue_analysis.sql

--- business_analytics/03_product_mix_analysis/sql/04_country_customer_revenue_analysis.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  customer_country,
-  COUNT(DISTINCT customer_id) AS customers,
-  SUM(revenue)                AS revenue,
-  AVG(revenue)                AS revenue_per_customer
-FROM dw.v_sales_enriched
-GROUP BY 1
-ORDER BY revenue DESC;
