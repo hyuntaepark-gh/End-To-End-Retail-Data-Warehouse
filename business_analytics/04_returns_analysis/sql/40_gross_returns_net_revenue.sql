bbd3014e38684fc060321332e20bcbd3bff9f886
Hyuntae Park <hyuntae.park.j@gmail.com>
2026-02-08 16:21:22 -0500

    Delete business_analytics/04_returns_analysis/sql/40_gross_returns_net_revenue.sql

--- business_analytics/04_returns_analysis/sql/40_gross_returns_net_revenue.sql
+++
@@ -1,5 +0,0 @@
-SELECT
-  SUM(revenue) FILTER (WHERE invoice_no NOT LIKE 'C%') AS gross_sales,
-  SUM(revenue) FILTER (WHERE invoice_no LIKE 'C%')     AS returns_value,
-  SUM(revenue)                                         AS net_revenue
-FROM dw.v_sales_enriched;
