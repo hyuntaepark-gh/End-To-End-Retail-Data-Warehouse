00b6bf34c31ee40be7bfc91bd577423644eb8643
Hyuntae Park <hyuntae.park.j@gmail.com>
2026-02-08 16:21:16 -0500

    Delete business_analytics/04_returns_analysis/sql/40_top_return_products_by_value.sql

--- business_analytics/04_returns_analysis/sql/40_top_return_products_by_value.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  stock_code,
-  COUNT(*)      AS return_lines,
-  SUM(revenue)  AS return_value
-FROM dw.v_sales_enriched
-WHERE invoice_no LIKE 'C%'
-GROUP BY 1
-ORDER BY return_value DESC
-LIMIT 20;
